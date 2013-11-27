fa4fb0d2112d3e27c3638ce90fc081ae2c55e836
Rick Veens <rickveens92@gmail.com>
2013-11-27 12:36:28 +0100

    InputLow toegevoegd.

---
+++ OOPA/InputLow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPA
+{
+    class InputLow : Input
+    {
+        public override Object Clone()
+        {
+            return new InputLow();
+        }
+
+        public override string getKey()
+        {
+            return "INPUT_HIGH";
+        }
+    }
+}
